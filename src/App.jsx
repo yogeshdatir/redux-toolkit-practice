751ae811e42b31d924a4790ef01cae363a41cf1e
Yogesh D <ydatir@entrata.com>
2024-11-22 17:16:17 +0530

    installed redux-toolkit, added store and a reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Provider store={store}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Provider>
   )
 }
 
---
+++ src/redux/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  todos: [],
+}
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.todos.push(action.payload)
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { addTodo } = todoSlice.actions
+
+export default todoSlice.reducer
